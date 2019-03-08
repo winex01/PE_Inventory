b007cc817fe5105eb2bc0a736033cf8cac5a5f2f
winnie <winnie131212592@gmail.com>
2019-03-09 06:30:35 +0800

    base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,9 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://peinventory.test/';
-// $config['base_url'] = 'http://192.168.3.80/inventory/';
-
+$config['base_url'] = 'http://localhost/peinventory';
 /*
 |--------------------------------------------------------------------------
 | Index File
